9175b79e067291001b4c749a6cc5e1ca42297044
sadaf2825 <sadafnikfar55@gmail.com>
2025-05-21 14:26:25 +0430

    Initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
-import tailwindcss from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
-export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer,
-  ],
-}
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
 
